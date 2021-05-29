7e1b0918cad7b257b142d484b01ac52a52468bb4
Coodii <Coodii@users.noreply.github.com>
2021-05-29 16:00:50 +0200

    Adding function research

--- Buttonanimation.js
+++ Buttonanimation.js
@@ -45,5 +45,54 @@ function newaction() {
 }
 
 
+var list = document.getElementById("output");
+var result = null
 
+//This method is searching the book
+function reserch(){
+	var book = document.getElementById("book");
+	var author = document.getElementById("author");
+
+	//checking that the inputs are not empty
+	if((book.validity.valueMissing) || (author.validity.valueMissing)){
+		alert("Merci de remplir les champs Nom du livre et Nom de l'auteur");
+	} else { list.innerHTML="";
+	fetch("https://www.googleapis.com/books/v1/volumes?q=" + book.value + "+inauthor:" + author.value)
+	.then(a => a.json())
+	.then(response =>{
+		for(var i=0; i<10; i++){
+			//initialize an image
+			var image = "<br><img class=image-resize src= image/unavailable.jpg>"
+			item = response.items[i];
+			//Getting the id
+			var id = "<p class = id> Id: " + item.id + "</p>"
+			//Getting the description
+			var text = item.volumeInfo.description
+			// Getting the title
+			var title = "<p class = title> Titre: " + item.volumeInfo.title + "</p>"
+			//Getting the 
+			var author = "<p class = author> Auteur: " + item.volumeInfo.authors + "</p>"
+			//initialise a saving button
+			var book = "'result" + i +"'"
+			var save = "<button name= save id='save" + i +"' onclick = saving(" + book + ")>Sauvegarde</button>"
+			
+			
+			// If the description of the book exists and put the characters length at 200.
+			if (item.volumeInfo.description || text.length > 200 ) {
+				text = "<p> Description: " + text.substring(0,200) + " ..." + "</p>"
+			} 	else {
+				text = "<p> Description: Information manquante </p>"
+			}
+			
+			// Get the image a the book from the API
+			if (item.volumeInfo.imageLinks){
+				image = "<br><img class=image-resize src=" + item.volumeInfo.imageLinks.thumbnail + ".jpg>"
+			} 
+			var books= title + id + author + text + image + save
+			result = "<div class = 'research' id='result" + i +"'>" + books + "</div>"
+			list.innerHTML += result	
+		}		
+})
+}
+}
   
